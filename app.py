4fb2005a4e5de99b2fabe206e2ff1b7dbbd20e27
michaellah <michael@michaellah.com>
2025-12-12 11:21:03 -0800

    add python dotenv

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 import os
+from dotenv import load_dotenv
 
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
+
+# Load environment variables from .env file
+load_dotenv()
 
 # This sample slack application uses SocketMode
 # For the companion getting started setup guide,
